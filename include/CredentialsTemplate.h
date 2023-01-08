f7cddd7da02130584e9a99a7ca195685e1326810
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2023-01-08 18:44:29 +0100

    Add credentials template file

---
+++ include/CredentialsTemplate.h
@@ -0,0 +1,25 @@
+/*******************************************************************
+ * IMPORTANT! Please follow the instructions in this file before
+ * you try to compile the project for the first time. This file
+ * is just a template, don't change the contents of this file.
+ * 
+ * You need to do the following:
+ * 1. Save this file as Credentials.h in include/ directory and
+ *    than close this file
+ * 2. Open Credentials.h file and enter your WiFi credentials 
+ * 3. Save this file and compile and upload the project in debug
+ *    mode to check if everything works as it should
+ * 
+ * Credentials.h is ignored by Git (check the contents of .gitignore
+ * file to confirm), so this way your credentials will be kept in
+ * your local version of the project. This way you can change the 
+ * project and publish your changes on Github, Gitlab, Bitbucket or
+ * your own Git server without leaking sensitive information.
+ *******************************************************************/
+
+#ifndef CREDENTIALS_H
+    #define CREDENTIALS_H
+    // Add your AP credentials
+    #define AP_SSID "Your SSID"
+    #define AP_PASSWORD "Your password"
+#endif
