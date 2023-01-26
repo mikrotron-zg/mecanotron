a2bea5402fbe5b723d5cd2558dfca4146bfa437c
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2023-01-26 19:50:09 +0100

    Update includes README

--- include/Debug.h
+++ include/Debug.h
@@ -2,7 +2,7 @@
  * This files contains macros and debugging variables. If you want
  * to turn debug mode on, place the line
  * #define DEBUG_MODE
- * in file main.cpp before the include call for this file, i.e.
+ * in file Configuration.h before the include call for this file, i.e.
  * #include "Debug.h"
  * If you want to turn debug mode off, comment out or delete the
  * DEBUG_MODE definition line:
